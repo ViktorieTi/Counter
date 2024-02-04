f8082c77b592c7c67df694385b404961ed760c2e
ViktorieTi <victorya_br@icloud.com>
2024-02-04 15:55:30 +0300

    Добавлена кнопка "Нажми меня" с функцией увеличения числа в поле лейбла "Значение счётчика: 0" при нажатии

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    
+    var clickCounter: Int = 0
+    
+    @IBAction func clickOfCounterButton(_ sender: Any) {
+        clickCounter += 1
+        counterLabel.text = ("Значение счётчика: \(clickCounter)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
