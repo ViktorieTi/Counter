258a897e9d2a9244ce4f12ec4471000a94084558
ViktorieTi <victorya_br@icloud.com>
2024-02-04 20:05:53 +0300

    Добавлено разделение кнопки нажми меня на кнопку с плюсом и кнопку с минусом. Добавлена кнопка обнуления результата. Дабавлено текстовое окно с историей изменений. Добавлена переменная даты - не локальная

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,14 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
+  
+    @IBOutlet weak var counterMinusButton: UIButton!
+    @IBOutlet weak var makeZeroButton: UIButton!
     
-    var clickCounter: Int = 0
+    @IBOutlet weak var counterPlusButton: UIButton!
     
-    @IBAction func clickOfCounterButton(_ sender: Any) {
+    @IBOutlet weak var changeInfoTextView: UITextView!
+    
+    var clickCounter: UInt = 0
+    
+    var date = Date ()
+   
+    @IBAction func clickOfCounterPlusButton(_ sender: Any) {
         clickCounter += 1
         counterLabel.text = ("Значение счётчика: \(clickCounter)")
+        changeInfoTextView.text.append("\n [\(date)] значение изменено на +1")
     }
+    
+    @IBAction func clickOfCounterMinusButton(_ sender: Any) {
+        if clickCounter != 0 {
+            clickCounter -= 1
+            counterLabel.text = ("Значение счётчика: \(clickCounter)")
+            changeInfoTextView.text.append("\n [\(date)] значение изменено на -1")
+        } else {
+            counterLabel.text = ("Значение счётчика: \(clickCounter)")
+            changeInfoTextView.text.append("\n [\(date)] попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction func clickOfMakeZeroButton(_ sender: Any) {
+        clickCounter = 0
+        counterLabel.text = ("Значение счётчика: \(clickCounter)")
+        changeInfoTextView.text.append("\n [\(date)] значение сброшено")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
